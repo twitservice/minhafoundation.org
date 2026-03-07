888031f4ee71df5182caba448298546971be72fe
asifulmamun <asifulmamun@gmail.com>
2026-03-07 15:30:00 +0600

    done

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,17 @@
-// import Image from "next/image";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../assets/img/logo.png";
 
 
 export default function Header() {
     return (
         <header className="absolute top-8 left-1/2 transform -translate-x-1/2 w-10/12 bg-background flex items-center justify-between p-4">
             <div className="flex w-full">
-                <div>1</div>
+                <div>
+                    <Link href="/">
+                        <Image src={Logo} alt="Logo" width={50} height={50} />
+                    </Link>
+                </div>
                 <div>2</div>
                 <div>3</div>
             </div>
